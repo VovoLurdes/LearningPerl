b938e9a5806166509113e67f3bb148eac09110d3
Flavio Rocha <fla@Debian.WORKGROUP>
2014-11-19 01:51:52 -0200

    Testando Git

---
+++ sin.pl
@@ -0,0 +1,14 @@
+use LWP::UserAgent;
+ 
+my $ua = LWP::UserAgent->new;
+$ua->timeout(10);
+$ua->env_proxy;
+ 
+my $response = $ua->get('http://search.cpan.org/');
+ 
+if ($response->is_success) {
+    print $response->decoded_content;  # or whatever
+}
+else {
+    die $response->status_line;
+}
